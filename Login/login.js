dbaccb16cf562077bb080f6d41aa75e6354cde47
oliv3r1 <oliver.suomela@metropolia.fi>
2022-12-07 13:56:42 +0200

    register nappia

--- Login/login.js
+++ Login/login.js
@@ -6,3 +6,12 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+var modal = document.getElementById('Btn1');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
