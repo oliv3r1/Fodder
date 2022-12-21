af2d1def6ac3a31f9c45cc5be2140a8f5710142f
“oliv3r1” <oliver.suomela@metropolia.fi>
2022-12-21 12:45:12 +0200

    server

--- Login/public/Etusivu/login/login.js
+++ Login/public/Etusivu/login/login.js
@@ -1,59 +1,50 @@
-'use strict';
+"use strict";
 
-var modal = document.getElementById('Btn');
-const info = document.getElementById('login-data');
-
+var modal = document.getElementById("Btn");
+const info = document.getElementById("login-data");
 
 document.onload = async () => {
   // const sneakers = await getAllSneakers();
   const getUserInfo = await getUser("jarmo");
-}
+};
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+};
 
-var modal = document.getElementById('Btn1');
+var modal = document.getElementById("Btn1");
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+};
 
-const url = 'http://localhost:3000'; // change url when uploading to server
-
-
-
+const url = "10.114.34.67/app/"; // change url when uploading to server
 
 const getUser = async (id) => {
-  const response = await fetch(url + '/user/' + id);
+  const response = await fetch(url + "/user/" + id);
   const user = await response.json();
   return user;
 };
 
 const userInfo = async () => {
-  const response = await fetch(url + '/user/me');
+  const response = await fetch(url + "/user/me");
   const user = await response.json();
   console.log(user);
-  info.innerText =  user.User;
+  info.innerText = user.User;
 };
 
 const logout = async () => {
-  const response = await fetch(url + '/user/logout');
+  const response = await fetch(url + "/user/logout");
   const user = await response.text();
   location.reload();
   alert("You have successfully logged out!");
-
 };
-
 
 console.log(info);
 userInfo();
-
-
-
